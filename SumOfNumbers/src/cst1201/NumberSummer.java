04fdd8eb0f18ecf95bda2a5a484ed08baef18e1f
Raffi Khatchadourian <rkhatchadourian@citytech.cuny.edu>
2015-03-18 15:56:54 -0400

    Added code.

---
+++ SumOfNumbers/src/cst1201/NumberSummer.java
@@ -0,0 +1,14 @@
+package cst1201;
+
+public class NumberSummer {
+
+    public static void main(String[] args) {
+        /*
+        Write a program that asks the user for a positive nonzero integer value. 
+        The program should use a loop to get the sum of all the integers from 1 
+        up to the number entered. For example, if the user enters 50, the loop 
+        will find the sum of 1, 2, 3, 4, ... 50.
+         */
+    }
+
+}
